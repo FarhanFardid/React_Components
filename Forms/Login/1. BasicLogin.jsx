c57b98c2975ee8521b9ae72a9c34d4f8f61ec325
FarhanFardid <farhanfardid009@gmail.com>
2024-06-30 13:58:40 +0600

    updates to Form folder

--- Forms/Login/1. BasicLogin.jsx
+++ Forms/Login/1. BasicLogin.jsx
@@ -1,122 +1,101 @@
+// Change Info and style as necessary.
 import { useContext, useState } from "react";
 import backgroundImage from "../../assets/images/farmerRegister/farmerRegister.jpg";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider/AuthProvider";
 const Login = () => {
-  const backgroundStyle = {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    display: "flex",
-    justifyContent: "end",
-    alignItems: "center",
-  };
-  const navigate = useNavigate();
-  const { login } = useContext(AuthContext);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [error, setError] = useState("");
-  const validateEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-      setEmailError("Please enter a valid email address.");
-    } else {
-      setEmailError("");
-    }
-  };
+    const backgroundStyle = {
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        display: 'flex',
+        justifyContent: 'end',
+        alignItems: 'center'
+    };
+    const navigate = useNavigate();
+    const { login } = useContext(AuthContext)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [error, setError] = useState('');
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            setEmailError('Please enter a valid email address.');
+        } else {
+            setEmailError('');
+        }
+    };
 
-  const handleEmailChange = (e) => {
-    const newEmail = e.target.value;
-    setEmail(newEmail);
-    validateEmail(newEmail);
-  };
+    const handleEmailChange = (e) => {
+        const newEmail = e.target.value;
+        setEmail(newEmail);
+        validateEmail(newEmail);
+    };
 
-  const handlePasswordChange = (e) => {
-    const newPassword = e.target.value;
-    setPassword(newPassword);
-    if (newPassword.length < 8) {
-      setPasswordError("Password must be at least 8 characters long.");
-    } else {
-      setPasswordError("");
-    }
-  };
+    const handlePasswordChange = (e) => {
+        const newPassword = e.target.value;
+        setPassword(newPassword);
+        if (newPassword.length < 8) {
+            setPasswordError('Password must be at least 8 characters long.');
+        } else {
+            setPasswordError('');
+        }
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const storedData = JSON.parse(localStorage.getItem("formData"));
-    if (
-      storedData &&
-      storedData.email === email &&
-      storedData.password === password
-    ) {
-      login();
-      alert("Login Successfully");
-      navigate("/home");
-    } else {
-      setError("Invalid email or password. Please try again.");
-      setPassword("");
-      setError("");
-    }
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const storedData = JSON.parse(localStorage.getItem('formData'));
+        if (storedData && storedData.email === email && storedData.password === password) {
+            login();
+            alert('Login Successfully')
+            navigate('/');
+        } else {
+            setError('Invalid email or password. Please try again.');
+            setPassword('');
+            setError('')
+        }
+    };
 
-  return (
-    <div className="text-6xl h-[100vh]" style={backgroundStyle}>
-      <div className="flex items-center justify-center xl:w-1/2 xl:my-0 w-full md:my-8">
-        <div className="bg-gradient-to-r from-lime-500 to-lime-700 border-solid rounded-2xl w-full md:w-[34rem] my-10 py-10">
-          <h1 className="text-center text-5xl font-bold text-emerald-900">
-            Login
-          </h1>
-          <div className="flex items-center justify-center mt-4 text-2xl text-white">
-            <form onSubmit={handleSubmit}>
-              <label>
-                <p className="mb-2"> Email:</p>
-                <input
-                  className="w-[18rem] md:w-[28rem] mb-2 text-black"
-                  type="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  required
-                />
-                {emailError && <p className="text-red-500">{emailError}</p>}
-              </label>
-              <br />
-              <label>
-                <p className="mb-2"> Password:</p>
-                <input
-                  className="w-[18rem] md:w-[28rem] mb-2 text-black"
-                  type="password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                />
-                {passwordError && (
-                  <p className="text-red-500">{passwordError}</p>
-                )}
-              </label>
-              <br />
-              {error && <p className="text-red-500 mb-4 text-xl">{error}</p>}
-              <div className="flex justify-center mt-2">
-                <button
-                  type="submit"
-                  className="bg-green-900 text-yellow-400 btn-md rounded-lg font-bold  hover:text-amber-200 hover:bg-green-500 h-8 w-[12rem] py-1 px-3 md:h-12 md:w-[16rem] md:py-3 md:px-6 text-xs md:text-base mr-5 hover:scale-110"
-                >
-                  Login
-                </button>
-              </div>
-              <div className="flex justify-center items-center gap-4 mt-4 text-xl font-normal">
-                <Link to="/authentication" className="text-green-900 font-bold">
-                  New User?
-                </Link>
-              </div>
-            </form>
-          </div>
+    return (
+        <div className=" h-[80vh] md:h-[100vh]" style={backgroundStyle}>
+            <div className="flex items-center justify-center  md:w-1/2 mt-5 md:mt-10 w-full md:my-8">
+                <div className="bg-gradient-to-r from-lime-500 to-lime-700 border-solid rounded-2xl my-5 md:my-10 py-7 md:py-10 w-[24rem] md:w-[34rem]">
+                    <h1 className="text-center text-2xl font-bold text-emerald-900 md:text-5xl py-3 md:py-7">Login</h1>
+                    <div className="flex items-center justify-center mt-4 text-2xl text-white ">
+                        <form onSubmit={handleSubmit} className="mx-auto">
+                            <label><p className="mb-2 text-base md:text-2xl font-semibold"> Email:</p>
+                                <input className="w-[20rem] md:w-[28rem] mb-4 md:mb-7 text-black rounded-lg" type="email" 
+                                placeholder="Enter your email"
+                                value={email}
+                                    onChange={handleEmailChange}
+                                    required />
+                                {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
+                            </label><br />
+                            <label><p className="mb-2 text-base md:text-2xl font-semibold"> Password:</p>
+                                <input className="w-[20rem] md:w-[28rem] mb-4 md:mb-7 text-black rounded-lg" type="password"
+                                  placeholder="Enter your password"
+                                    value={password}
+                                    onChange={handlePasswordChange}
+                                    required />
+                                {passwordError && <p className="text-red-500 text-sm">{passwordError}</p>}
+                            </label><br />
+                            {error && <p className="text-red-500 mb-4 text-sm">{error}</p>}
+                            <div className="flex justify-center mt-2">
+                                <button type="submit" className="bg-green-900 text-yellow-400 btn-md rounded-lg font-bold  hover:text-amber-200 hover:bg-green-500 h-8 w-14 py-1 px-3 md:h-12 md:w-24 md:py-3 md:px-6 text-xs md:text-base mr-5 hover:scale-110">Login</button>
+                            </div>
+                            <div className="mt-4 text-sm md:text-base font-medium p-3 md:p-5">
+                                <p> New to Rural ArgoCommerce?   <Link to="/authentication" className="text-green-900 font-bold text-base md:text-lg">Please Register</Link></p>
+                            
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
